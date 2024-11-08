f060900aeb20df73fe24026f86a71040b84965d3
itwaman <walter.mantovani@it.abb.com>
2024-11-08 19:00:12 +0100

    modified:   DOStatusMonitor.ps1

--- DOStatusMonitor.ps1
+++ DOStatusMonitor.ps1
@@ -2,16 +2,26 @@ while ($true) {
     Clear-Host;
     Get-DeliveryOptimizationStatus | Select-Object  PredefinedCallerApplication, 
             FileSize, 
-            TotalBytesDownloaded,
-            DownloadMode,
+            #TotalBytesDownloaded,
+            @{Name='TotBytesDL';Expression={$_.TotalBytesDownloaded}},
+            #DownloadMode,
+            @{Name='DL-Mode';Expression={$_.DownloadMode}},
             Status,
-            BytesFromHTTP,
-            BytesFromPeers,
-            BytesToLanPeers,
+            #BytesFromHTTP,
+            @{Name='From-HTTP';Expression={$_.BytesFromHTTP}},
+            @{Name='From-P2P';Expression={$_.BytesFromPeers}},
+            #BytesFromPeers,
+            #BytesFromCacheServer,
+            @{Name='From-Cache';Expression={$_.BytesFromCacheServer}},
+            #BytesToLanPeers,
+            @{Name='To-P2P';Expression={$_.BytesToLanPeers}},
             NumPeers,
-            PercentPeerCaching,
+            #PercentPeerCaching,
+            @{Name='%P2P-Caching';Expression={$_.PercentPeerCaching}},
             isPinned,
             ExpireOn,
-            FileId | Sort-Object PredefinedCallerApplication,FileSize |  Format-Table -Property * -AutoSize ;
+            #FileId `
+            @{Name='FileId';Expression={($_.FileId).replace('501FCB7D-A970-4E34-A753-4B48FE5D8BEF_dd525774-388a-4c66-a922-cb9335cab92e_','')}}`
+            | Sort-Object PredefinedCallerApplication,FileSize |  Format-Table -Property * -AutoSize ;
     Start-Sleep -Seconds 2
 }
