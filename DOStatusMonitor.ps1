9b36543ecfefbc7d480fdde1c7e12eaa62253893
itwaman <walter.mantovani@it.abb.com>
2025-05-22 12:49:13 +0200

    Adding CacheHost column

--- DOStatusMonitor.ps1
+++ DOStatusMonitor.ps1
@@ -20,6 +20,7 @@ while ($true) {
             @{Name='%P2P-Caching';Expression={$_.PercentPeerCaching}},
             isPinned,
             ExpireOn,
+            CacheHost,
             #FileId `
             @{Name='FileId';Expression={($_.FileId).replace('501FCB7D-A970-4E34-A753-4B48FE5D8BEF_dd525774-388a-4c66-a922-cb9335cab92e_','')}}`
             | Sort-Object PredefinedCallerApplication,FileSize |  Format-Table -Property * -AutoSize ;
